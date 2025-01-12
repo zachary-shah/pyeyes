ac65e15df135da55a41b45334e62ce3513ef9dbb
yonatanu <yonatanurman@gmail.com>
2025-01-11 17:51:11 -0800

    formattings, sorting, pre commit

--- tests/compare_diffusion.py
+++ tests/compare_diffusion.py
@@ -1,29 +1,28 @@
 import numpy as np
+
 from pyeyes.viewers import ComparativeViewer
 
 # Data
-festive_pth = '/local_mount/space/mayday/data/users/zachs/zachplotlib/data/dwi/recon_festive.npy'
-skope_pth = '/local_mount/space/mayday/data/users/zachs/zachplotlib/data/dwi/recon_skope.npy'
-uncorr_pth = '/local_mount/space/mayday/data/users/zachs/zachplotlib/data/dwi/recon_uncorr.npy'
+festive_pth = (
+    "/local_mount/space/mayday/data/users/zachs/zachplotlib/data/dwi/recon_festive.npy"
+)
+skope_pth = (
+    "/local_mount/space/mayday/data/users/zachs/zachplotlib/data/dwi/recon_skope.npy"
+)
+uncorr_pth = (
+    "/local_mount/space/mayday/data/users/zachs/zachplotlib/data/dwi/recon_uncorr.npy"
+)
 
-festive = np.load(festive_pth) # Bdir x X x Y x Z
+festive = np.load(festive_pth)  # Bdir x X x Y x Z
 skope = np.load(skope_pth)
 uncorr = np.load(uncorr_pth)
 
-img_dict = {
-    'skope': skope,
-    'festive': festive,
-    'uncorr': uncorr
-}
+img_dict = {"skope": skope, "festive": festive, "uncorr": uncorr}
 
 # Parameters
-named_dims = ['Bdir', 'x', 'y', 'z']
-vdims = ['x', 'y']
+named_dims = ["Bdir", "x", "y", "z"]
+vdims = ["x", "y"]
 
-Viewer = ComparativeViewer(
-    data = img_dict,
-    named_dims = named_dims,
-    view_dims = vdims
-)
+Viewer = ComparativeViewer(data=img_dict, named_dims=named_dims, view_dims=vdims)
 
 Viewer.launch()
--- tests/test_dti.py
+++ tests/test_dti.py
@@ -1,21 +1,21 @@
-
-import numpy as np
 import matplotlib.pyplot as plt
+import numpy as np
 from matplotlib import use as mpl_backend
+
 from pyeyes.mpl.dti import plot_dti
 
-mpl_backend('WebAgg')
+mpl_backend("WebAgg")
 
 # Options.
-dti_type = 'festive' # 'skope', 'uncorr', or 'festive'
+dti_type = "festive"  # 'skope', 'uncorr', or 'festive'
 
 # TODO: allow multiple DTI to compare like with ComparativeViewer. This will need to handle color dims tho
-dti_root = '/local_mount/space/mayday/data/users/zachs/zachplotlib/data/dti/dtifit'
+dti_root = "/local_mount/space/mayday/data/users/zachs/zachplotlib/data/dti/dtifit"
 dti_folder = f"{dti_root}/{dti_type}/{dti_type}_b1000_DTI"
 
 Viewer = plot_dti(
-    dti_imgs_or_path = dti_folder,
-    suptitle = f"DTI: {dti_type.capitalize()}",
+    dti_imgs_or_path=dti_folder,
+    suptitle=f"DTI: {dti_type.capitalize()}",
 )
 
 Viewer.launch()
