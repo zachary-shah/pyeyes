use correct formula for phase error maps

src/pyeyes/metrics.py
@@ -21,11 +21,30 @@ MAPPABLE_METRICS = [
 TOL = 1e-5
 
 
-def L1Diff(recon: np.ndarray, true: np.ndarray, return_map=False) -> float:
+def diff(recon: np.ndarray, true: np.ndarray, return_map=False, isphase=False) -> float:
 
     assert recon.shape == true.shape, "Input array dimensions mismatch."
 
-    l1_diff = np.abs(recon - true)
+    if isphase:
+        diff = np.angle(np.exp(1j * recon) / np.exp(1j * true))
+    else:
+        diff = recon - true
+
+    if return_map:
+        return diff
+
+    return np.mean(diff)
+
+
+def L1Diff(
+    recon: np.ndarray, true: np.ndarray, return_map=False, isphase=False
+) -> float:
+
+    assert recon.shape == true.shape, "Input array dimensions mismatch."
+
+    diff_map = diff(recon, true, return_map=True, isphase=isphase)
+
+    l1_diff = np.abs(diff_map)
 
     if return_map:
         l1_diff[l1_diff < TOL] = np.nan
@@ -34,7 +53,25 @@ def L1Diff(recon: np.ndarray, true: np.ndarray, return_map=False) -> float:
     return np.mean(l1_diff)
 
 
-def RMSE(recon: np.ndarray, true: np.ndarray, return_map=False) -> float:
+def RelativeL1(
+    recon: np.ndarray, true: np.ndarray, return_map=False, isphase=False
+) -> float:
+
+    assert recon.shape == true.shape, "Input array dimensions mismatch."
+
+    diff_map = diff(recon, true, return_map=True, isphase=isphase)
+
+    rel_diff = np.abs(diff_map) / (np.abs(true) + 1e-9)
+
+    if return_map:
+        return rel_diff
+
+    valid = np.abs(true) > TOL
+
+    return np.mean(rel_diff[valid])
+
+
+def RMSE(recon: np.ndarray, true: np.ndarray, return_map=False, isphase=False) -> float:
     """
     alias for L2Diff
 
@@ -45,7 +82,9 @@ def RMSE(recon: np.ndarray, true: np.ndarray, return_map=False) -> float:
 
     assert recon.shape == true.shape, "Input array dimensions mismatch."
 
-    mse = np.abs(recon - true) ** 2
+    diff_map = diff(recon, true, return_map=True, isphase=isphase)
+
+    mse = np.abs(diff_map) ** 2
 
     if return_map:
         mse = np.sqrt(mse)
@@ -55,7 +94,9 @@ def RMSE(recon: np.ndarray, true: np.ndarray, return_map=False) -> float:
     return np.sqrt(np.mean(mse))
 
 
-def NRMSE(recon: np.ndarray, true: np.ndarray, return_map=False, eps=1e-8) -> float:
+def NRMSE(
+    recon: np.ndarray, true: np.ndarray, return_map=False, isphase=False, eps=1e-8
+) -> float:
     """
     Given 2 complex arrays of the same shape, recon (g) and true (f), compute the NRMSE between arrays
         NRMSE = SQRT(|f-g|^2 / |f|^2)
@@ -63,7 +104,9 @@ def NRMSE(recon: np.ndarray, true: np.ndarray, return_map=False, eps=1e-8) -> fl
 
     assert recon.shape == true.shape, "Input array dimensions mismatch."
 
-    mse = np.abs(recon - true) ** 2
+    diff_map = diff(recon, true, return_map=True, isphase=isphase)
+
+    mse = np.abs(diff_map) ** 2
 
     if return_map:
         nrmse = mse / ((np.abs(true) ** 2) + eps)
@@ -76,13 +119,20 @@ def NRMSE(recon: np.ndarray, true: np.ndarray, return_map=False, eps=1e-8) -> fl
 
 
 def SSIM(
-    recon: np.ndarray, true: np.ndarray, return_map=False, percentiles=[0.5, 99.5]
+    recon: np.ndarray,
+    true: np.ndarray,
+    return_map=False,
+    isphase=False,
+    percentiles=[0.5, 99.5],
 ) -> float:
     """
     Compute the Structural Similarity Index (SSIM) between two images.
     """
 
     assert recon.shape == true.shape, "Input array dimensions mismatch."
+
+    if isphase:
+        pass  # do nothing different, but SSIM doesn't really make sense for phase maps
 
     true_abs = np.abs(true)
     upper_bound = np.percentile(true_abs, percentiles[1])
@@ -96,7 +146,9 @@ def SSIM(
     return compare_ssim(recon, true, data_range=data_range)
 
 
-def PSNR(recon: np.ndarray, true: np.ndarray, max_percentile=99.5) -> float:
+def PSNR(
+    recon: np.ndarray, true: np.ndarray, isphase=False, max_percentile=99.5
+) -> float:
     """
     Compute the Peak Signal-to-Noise Ratio (PSNR) between two images.
     """
@@ -105,37 +157,11 @@ def PSNR(recon: np.ndarray, true: np.ndarray, max_percentile=99.5) -> float:
 
     max_val = np.percentile(np.abs(true), max_percentile)
 
-    mse = RMSE(recon, true)
+    mse = RMSE(recon, true, isphase=isphase)
 
     psnr = 20 * np.log10(max_val / mse)
 
     return psnr
-
-
-def RelativeL1(recon: np.ndarray, true: np.ndarray, return_map=False) -> float:
-
-    assert recon.shape == true.shape, "Input array dimensions mismatch."
-
-    rel_diff = np.abs(recon - true) / (np.abs(true) + 1e-9)
-
-    if return_map:
-        return rel_diff
-
-    valid = np.abs(true) > TOL
-
-    return np.mean(rel_diff[valid])
-
-
-def diff(recon: np.ndarray, true: np.ndarray, return_map=False) -> float:
-
-    assert recon.shape == true.shape, "Input array dimensions mismatch."
-
-    diff = recon - true
-
-    if return_map:
-        return diff
-
-    return np.mean(diff)
 
 
 METRIC_CALLABLES = {