e212a768d73e11991634425d37a703387eeee4af
zshah9 <zshah9@stanford.edu>
2026-01-21 10:11:14 -0800

    move prototypes in __init__

--- src/pyeyes/__init__.py
+++ src/pyeyes/__init__.py
@@ -1,7 +1,8 @@
 from .app import launch_viewers
-from .line import launch_1d_viewer
 from .themes import set_theme
 from .viewers import ComparativeViewer, spawn_comparative_viewer_detached
+
+from .prototypes.line import launch_1d_viewer
 
 # Default Dark Theme
 set_theme("dark")
