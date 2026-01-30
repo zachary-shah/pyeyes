e0aa85a163ab7789e498e49a77325741b22e028b
zshah9 <zshah9@stanford.edu>
2026-01-30 10:29:48 -0800

    Upate inits and docstrings for mkdoc

--- src/pyeyes/__init__.py
+++ src/pyeyes/__init__.py
@@ -1,5 +1,4 @@
-from .app import launch_viewers
-from .prototypes.line import launch_1d_viewer
+from .app import launch_viewers, launch_comparative_viewer
 from .themes import set_theme
 from .viewers import ComparativeViewer, spawn_comparative_viewer_detached
 
---
+++ src/pyeyes/cmap/__init__.py
@@ -0,0 +1,17 @@
+from .cmap import (
+    ColorMap,
+    QuantitativeColorMap,
+    VALID_COLORMAPS,
+    VALID_ERROR_COLORMAPS,
+    QUANTITATIVE_MAPTYPES,
+    relaxation_color_map,
+    color_log_remap,
+)
+
+__all__ = [
+    "ColorMap",
+    "QuantitativeColorMap",
+    "VALID_COLORMAPS",
+    "VALID_ERROR_COLORMAPS",
+    "QUANTITATIVE_MAPTYPES",
+]
